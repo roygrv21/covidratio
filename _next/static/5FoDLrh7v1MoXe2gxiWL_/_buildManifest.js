fefd0125604961220605424666bd13e569c9d2fd
davidbarratt <davidbarratt@users.noreply.github.com>
2020-08-22 23:56:56 +0000

    deploy: 4121bf922486b315f91ff4288fe1f98582849060

---
+++ _next/static/5FoDLrh7v1MoXe2gxiWL_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fcss\u002F75733bfb9ba4c5ff8b0d.css","static\u002Fchunks\u002Fpages\u002Findex-5b363d52c16357791044.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fa39c7ce12d3ae2cec40.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
